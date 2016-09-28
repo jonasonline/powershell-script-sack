0268ab2cdf5f2a24a534c9c2b9bf2eec80797c83
Jonas Carlsson <jonas@jonasonline.se>
2016-09-28 16:15:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# powershell-script-sack
+Useful powershell scripts
+
+## QueryDatabaseAndSendMail.ps1
+Does pretty much what it says.
+
+## Remove-OldAzureStorageBlob.ps1
+Does every step needed to remove Azure blobs, e.g. sets a context, connects a container and deletes blobs based on date. 
---
+++ Remove-OldAzureStorageBlob.ps1
@@ -0,0 +1,17 @@
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True)]
+   [string]$StorageAccountName,
+	
+   [Parameter(Mandatory=$True)]
+   [string]$StorageAccountKey,
+
+   [Parameter(Mandatory=$True)]
+   [string]$StorageContainerName,
+
+   [Parameter(Mandatory=$True)]
+   [date]$OlderThanDate
+)
+$context = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+$container = Get-AzureStorageContainer -Name $StorageContainerName -Context $context
+Get-AzureStorageBlob -Container $container.Name -Context $context | Where-Object {$_.LastModified -lt $OlderThanDate} | Remove-AzureStorageBlob
