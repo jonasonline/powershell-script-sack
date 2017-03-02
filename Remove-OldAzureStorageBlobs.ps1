1ba1ef3dd48f722655b8b081be43db22f20f7861
Jonas Carlsson <jonas.carlsson@atea.se>
2017-03-02 14:22:45 +0100

    added batching to blob removal

--- Remove-OldAzureStorageBlobs.ps1
+++ Remove-OldAzureStorageBlobs.ps1
@@ -12,6 +12,17 @@ Param(
    [Parameter(Mandatory=$True)]
    [Int]$CleanupTime
 )
+
+$maxReturn = 5000
+$total = 0
+$token = $Null
 $context = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
 $container = Get-AzureStorageContainer -Name $StorageContainerName -Context $context
-Get-AzureStorageBlob -Container $container.Name -Context $context | Where-Object {$_.LastModified -lt (Get-Date).AddHours(-$CleanupTime)} | Remove-AzureStorageBlob
+do {
+  $blobs = Get-AzureStorageBlob -Container $container.Name -Context $context -ContinuationToken $token -MaxCount $maxReturn 
+  Write-Output "Fetched $($blobs.Length) blob records."
+  if($blobs.Length -le 0) { Break;}
+  $token = $blobs[$blobs.Count -1].ContinuationToken;
+  Write-Output "Removing $($($blobs | Where-Object {$_.LastModified -lt (Get-Date).AddHours(-$CleanupTime)} | Measure-Object).Count) blob records."
+  $blobs | Where-Object {$_.LastModified -lt (Get-Date).AddHours(-$CleanupTime)} | Remove-AzureStorageBlob
+} while ($token -ne $Null)
